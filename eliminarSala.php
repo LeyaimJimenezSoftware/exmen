612cd690469d3c6bc161f6fd46c925824011d2e1
Leyaim Jimenez <leyaim.jimenez@michelada.io>
2018-12-10 12:23:37 -0600

    Issue #2 & issie #3

--- eliminarSala.php
+++ eliminarSala.php
@@ -1,5 +1,8 @@
 <?php session_start(); 
 //datos para establecer la conexion con la base de mysql.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require "cfg/conexion.php";
 
 // verificamos si se han enviado ya las variables necesarias.
@@ -7,8 +10,8 @@ if (isset($_GET["id"])) {
     $id = $_GET["id"];
 
 
-                $query = 'DELETE  SALA_REMOTA WHERE idsala_remota='.$id;
-                mysql_query($query) or die(mysql_error());
+                $query = 'DELETE FROM SALA_REMOTA WHERE id_sala_remota='.$id;
+                mysqli_query($mysqli,$query) or die(mysql_error());
 }
 
    header('Location: index.php');
--- index.php
+++ index.php
@@ -17,9 +17,6 @@
 
     <!-- MetisMenu CSS -->
     <link href="css/plugins/metisMenu/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/sb-admin-2.css" rel="stylesheet">
  
 
 </head>
@@ -55,18 +52,9 @@ require "cfg/conexion.php";
    }
    echo "</table>";
 ?>
-   <button type="button" class="btn btn-success" onclick="window.location.href='sala_remota.html'">Angregar Sala</button>
+   <button type="button" class="btn btn-success" onclick="window.location.href='sala_remota.html'">Agregar Sala</button>
    </div>
-    <!-- jQuery -->
-    <script src="../js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../js/plugins/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../.js/sb-admin-2.js"></script>
+   
+    
 
 </body>
